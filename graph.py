0e15ce0bf0b75213f9b434fc0892ca4ea536e52f
needleworm <bhban@kakao.com>
2019-09-09 12:04:54 +0900

    please..

--- graph.py
+++ graph.py
@@ -15,15 +15,71 @@ class FNN():
     def graph(self, reset, logdir):
         if reset:
             model = tf.keras.Sequential()
-            model.add(layers.Dense(128, activation="relu"))
+            model.add(layers.Dense(60, activation="relu"))
             model.add(layers.BatchNormalization())
-            model.add(layers.Dense(128, activation="relu"))
+            model.add(layers.Dense(60, activation="relu"))
             model.add(layers.BatchNormalization())
-            model.add(layers.Dense(128, activation="relu"))
+            model.add(layers.Dense(30, activation="relu"))
             model.add(layers.BatchNormalization())
-            model.add(layers.Dense(128, activation="relu"))
+            model.add(layers.Dense(20, activation="relu"))
             model.add(layers.BatchNormalization())
-            model.add(layers.Dense(self.Y_size, activation="relu"))
+            model.add(layers.Dense(self.Y_size, activation="sigmoid"))
+            model.compile(optimizer=self.optimizer,
+                          loss=self.loss,
+                          metrics=self.metrics)
+        else:
+            model = keras.models.load_model(logdir+"/saved_model.h5")
+        return model
+
+
+class FNN2():
+    def __init__(self, X_size, Y_size, loss, optimizer, metrics, reset, logdir):
+        self.X_size = X_size
+        self.Y_size = Y_size
+        self.loss = loss
+        self.optimizer = optimizer
+        self.metrics = metrics
+        self.GRAPH = self.graph(reset, logdir)
+
+    def graph(self, reset, logdir):
+        if reset:
+            model = tf.keras.Sequential()
+            model.add(layers.Dense(60, activation="relu"))
+            model.add(layers.Dense(30, activation="relu"))
+            model.add(layers.Dense(self.Y_size, activation="sigmoid"))
+            model.compile(optimizer=self.optimizer,
+                          loss=self.loss,
+                          metrics=self.metrics)
+        else:
+            model = keras.models.load_model(logdir+"/saved_model.h5")
+        return model
+
+
+class FNN3():
+    def __init__(self, X_size, Y_size, loss, optimizer, metrics, reset, logdir):
+        self.X_size = X_size
+        self.Y_size = Y_size
+        self.loss = loss
+        self.optimizer = optimizer
+        self.metrics = metrics
+        self.GRAPH = self.graph(reset, logdir)
+
+    def graph(self, reset, logdir):
+        if reset:
+            model = tf.keras.Sequential()
+            model.add(layers.Dense(60, activation="relu"))
+            model.add(layers.BatchNormalization())
+            model.add(layers.Dense(60, activation="relu"))
+            model.add(layers.BatchNormalization())
+            model.add(layers.Dense(30, activation="relu"))
+            model.add(layers.BatchNormalization())
+            model.add(layers.Dense(30, activation="relu"))
+            model.add(layers.BatchNormalization())
+            model.add(layers.Dense(30, activation="relu"))
+            model.add(layers.BatchNormalization())
+            model.add(layers.Dense(20, activation="relu"))
+            model.add(layers.BatchNormalization())
+            model.add(layers.Dense(self.Y_size, activation="sigmoid"))
             model.compile(optimizer=self.optimizer,
                           loss=self.loss,
                           metrics=self.metrics)
--- main.py
+++ main.py
@@ -3,14 +3,16 @@ Byunghyun Ban
 bhban@kaist.ac.kr
 halfbottle@sangsang.farm
 """
-
+import os
+import tensorflow as tf
 import tensorflow as tf
 import numpy as np
 import graph
-import os
 import shutil
 from tensorflow import keras
 import time
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 # Flag Set
 FLAGS = tf.flags.FLAGS
@@ -19,25 +21,24 @@ tf.flags.DEFINE_string('mode', "train", "mode : train/ predict [default : train]
 tf.flags.DEFINE_string('device', '/gpu:0', "device : /cpu:0 /gpu:0 [default : /gpu:0]")  # Device
 tf.flags.DEFINE_bool('reset', "True", "reset : True/False")  # Reset train history
 tf.flags.DEFINE_integer("num_X", "60", "SIZE. [Default : 60]") # size of input data
-tf.flags.DEFINE_integer("num_Y", "17", "SIZE. [Default : 17]") # size of label
-tf.flags.DEFINE_integer("training_batch_size", "512", "batch size for training. [default : 128]")
-tf.flags.DEFINE_integer("test_batch_size", "512", "batch size for validation. [default : 128]")
-tf.flags.DEFINE_integer("predict_batch_size", "512", "batch size for visualization. [default : 128]")
-tf.flags.DEFINE_integer("num_epochs", "20", "how many epochs?. [default : 12]")
+tf.flags.DEFINE_integer("num_Y", "16", "SIZE. [Default : 17]") # size of label
+tf.flags.DEFINE_integer("training_batch_size", "64", "batch size for training. [default : 128]")
+tf.flags.DEFINE_integer("test_batch_size", "64", "batch size for validation. [default : 128]")
+tf.flags.DEFINE_integer("predict_batch_size", "64", "batch size for visualization. [default : 128]")
+tf.flags.DEFINE_integer("num_epochs", "10", "how many epochs?. [default : 12]")
 
 # Directory Setting
 logs_dir = "logs"
-test_x = "data/test_X.npy"
-test_y = "data/test_Y.npy"
-training_x = "data/training_X.npy"
-training_y = "data/training_Y.npy"
+test_x = "data/normalized_test_X.npy"
+test_y = "data/normalized_test_Y.npy"
+training_x = "data/normalized_train_X.npy"
+training_y = "data/normalized_train_Y.npy"
 
 # Hyperparameters
-dropout_rate = 0.5
 learning_rate = 1e-4
 
 # Define Model
-MODEL = graph.FNN
+MODEL = graph.FNN3
 
 # Directory Reset
 if FLAGS.mode is "predict":
@@ -84,13 +85,15 @@ def main():
         model.GRAPH.fit(Train_X, Train_Y,
                     epochs=FLAGS.num_epochs,
                     batch_size=FLAGS.training_batch_size,
-                    callbacks=[tensorboard])
-        print("** Training Done **")
-        print(model.GRAPH.summary())
+                    callbacks=[tensorboard],
+                    validation_data=(Test_X, Test_Y))
 
     test_loss, test_acc = model.GRAPH.evaluate(Test_X, Test_Y)
     print("** test loss is : " + str(test_loss))
     print("** test acc is : " + str(test_acc))
+
+    print("** Training Done **")
+    print(model.GRAPH.summary())
 
     model.GRAPH.save(logs_dir + "/saved_model.h5")
 
